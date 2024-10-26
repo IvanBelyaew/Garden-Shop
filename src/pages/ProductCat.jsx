2653e64dbd2ac94efae509a0321e66fba72b4de4
IvanBelyaew <131029557+IvanBelyaew@users.noreply.github.com>
2024-10-27 00:50:07 +0300

    Add files via upload

---
+++ src/pages/ProductCat.jsx
@@ -0,0 +1,8 @@
+const ProductCat = () => {
+  return (
+    <div>
+      <h1>Товары из категории</h1>
+    </div>
+  );
+};
+export { ProductCat };
---
+++ src/pages/Products.jsx
@@ -0,0 +1,18 @@
+import { useLocation } from "react-router-dom";
+import ShowProducts from "../components/ShowProducts/ShowProducts";
+import styles from "./Products.module.css";
+
+const Products = () => {
+  const location = useLocation();
+  const [catId, catTitle] = location.search.slice(1).split("&");
+  let checkcatTitle = catTitle.replace(/%20/g, " ");
+  return (
+    <>
+      <div className={styles.prod}>
+        <h1>{checkcatTitle}</h1>
+      </div>
+      <ShowProducts catid={catId}></ShowProducts>
+    </>
+  );
+};
+export { Products };
